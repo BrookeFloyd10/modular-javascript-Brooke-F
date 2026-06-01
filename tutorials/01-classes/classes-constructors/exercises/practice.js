2cc556cc88d57c16065a241d895a08a79856d9d7
Brooke F <brooke.floyd10@gmail.com>
2026-06-01 10:07:37 -0500

    completed practice exercise, added new classes and instances, logged items, fixed broken code

--- tutorials/01-classes/classes-constructors/exercises/practice.js
+++ tutorials/01-classes/classes-constructors/exercises/practice.js
@@ -12,13 +12,24 @@
     1. Give it a constructor that accepts two parameters: 'itemName' and 'goldValue'.
     2. Inside the constructor, map those parameters to 'this.name' and 'this.value'.
 */
-
+class GameItem {
+    constructor(itemName, goldValue) {
+        this.item = itemName;
+        this.value = goldValue;
+    }
+}
 /*
     TODO: Instantiate two unique items using the 'new' keyword:
     1. Create a constant named 'potion' with a name of "Healing Potion" and a value of 50.
     2. Create a constant named 'shield' with a name of "Iron Shield" and a value of 150.
     Log both objects to verify their data structures.
 */
+ const potion = new GameItem("Healing Potion", 50);
+ const shield = new GameItem("IronShield", 150);
+ 
+ console.log(`\n---- New Items Aquired ----`)
+ console.log(potion);
+ console.log(shield);
 
 /** EXERCISE 2: MOB SPAWNER VERIFICATION **/
 
@@ -33,6 +44,6 @@ class EnemyMob {
     TODO: The instantiation sequence below is completely broken and throwing errors!
     Fix the code line below so that a new EnemyMob instance is successfully generated.
 */
-const eliteOrc = EnemyMob("Orc Raider", 45);
+const eliteOrc = new EnemyMob("Orc Raider", 45);
 
 console.log(`\nA hostile ${eliteOrc.mobType} has appeared! (Power: ${eliteOrc.power})`);
