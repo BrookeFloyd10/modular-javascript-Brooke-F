88b0fd059696f096fc4762bb90e3fbb2faad0cfa
Brooke F <brooke.floyd10@gmail.com>
2026-06-03 13:36:07 -0500

    completed demo with video

--- tutorials/02-modules/commonjs-modules/exercises/demo/main.js
+++ tutorials/02-modules/commonjs-modules/exercises/demo/main.js
@@ -1,11 +1,12 @@
 /**** COMMONJS MODULES ****/
 
 // DEMO: Import the assets from gameData.js
+const dataBundle = require("./gameData.js");
 
 // DEMO: Replace null values below with the correct references to the imported assets
 console.log("--- INITIALIZING ARCADE ASSETS ---");
-console.log(`Configuring system... Default Player Health: ${null}`);
-console.log(`Spawning equipment... Starter weapon designated: ${null}`);
+console.log(`Configuring system... Default Player Health: ${dataBundle.startingHealthValue}`);
+console.log(`Spawning equipment... Starter weapon designated: ${dataBundle.defaultWeaponName}`);
 
 /* 
     Follow up with additional exercises after demo for hands-on practice 
